dc4eaca8a917460fdf18daad9ee7f9ec7b714e82
xiangshifu <2399770210@qq.com>
2018-10-27 14:54:59 +0800

    添加了一个lib包

--- app/src/main/java/financer/kuaishoudan/com/myapplication/MainActivity.java
+++ app/src/main/java/financer/kuaishoudan/com/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package financer.kuaishoudan.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import financer.kuaishoudan.com.mylibrary.Test;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, " result : " + Test.add(10,11), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ mylibrary/src/main/java/financer/kuaishoudan/com/mylibrary/Test.java
@@ -0,0 +1,12 @@
+package financer.kuaishoudan.com.mylibrary;
+
+/**
+ * Create by Xiangshifu
+ * on 2018/10/27 2:52 PM
+ */
+public  class Test {
+
+    public static int add(int a,int b){
+        return  a+b;
+    }
+}
